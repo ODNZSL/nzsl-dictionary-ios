1e5a32803839d0389196b1a8de003b4ea62ff89d
Eoin Kelly <eoin@eoinkelly.info>
2016-01-15 13:23:28 +1300

    Rename module name from `NZSL Dict` to `NZSLDict`
    This rename means we don't have to deal with spaces in paths and the name
    of the bridging header used for swift can be directly mapped to the module
    name.

---
+++ NZSLDict/Classes/NZSLDict-Bridging-Header.h
@@ -0,0 +1,11 @@
+//
+//  Use this file to import your target's public headers that you would like to expose to Swift.
+//
+
+#import "Dictionary.h"
+#import "DiagramViewController.h"
+#import "DetailViewController.h"
+#import "SearchViewController.h"
+#import "VideoViewController.h"
+#import "HistoryViewController.h"
+#import "imageutil.h"
