3cd8827ed46a5e9f2697abff86543f9b333e356e
Josh McArthur <joshua.mcarthur@gmail.com>
2017-06-09 15:35:15 +1200

    Remove about content from phone viewcontroller

--- NZSLDict/Classes/ViewControllerPhone.swift
+++ NZSLDict/Classes/ViewControllerPhone.swift
@@ -5,7 +5,6 @@ class ViewControllerPhone: UITabBarController, ViewControllerDelegate, SearchVie
     var diagramController: DiagramViewController!
     var videoController: VideoViewController!
     var historyController: HistoryViewController!
-    var aboutController: AboutViewController!
 
     override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: NSBundle?) {
         super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
@@ -22,14 +21,12 @@ class ViewControllerPhone: UITabBarController, ViewControllerDelegate, SearchVie
         diagramController = DiagramViewController()
         videoController = VideoViewController()
         historyController = HistoryViewController()
-        aboutController = AboutViewController()
 
         self.viewControllers = [
             searchController,
             diagramController,
             videoController,
             historyController,
-            aboutController
         ]
 
         searchController.delegate = self
@@ -38,18 +35,7 @@ class ViewControllerPhone: UITabBarController, ViewControllerDelegate, SearchVie
         historyController.delegate = self
     }
 
-    // TODO: I'm not 100% that this old implementation can be replaced by what I did below
-//- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)toInterfaceOrientation
-//{
-//    return toInterfaceOrientation == UIInterfaceOrientationPortrait
-//        || UIInterfaceOrientationIsLandscape(toInterfaceOrientation);
-//}
-//
-//    override func shouldAutorotateToInterfaceOrientation(toInterfaceOrientation: UIInterfaceOrientation) -> Bool {
-//            return toInterfaceOrientation == UIInterfaceOrientation.Portrait
-//        || UIInterfaceOrientationIsLandscape(toInterfaceOrientation);
-//    }
-    override func shouldAutorotate() -> Bool {
+       override func shouldAutorotate() -> Bool {
         return true
     }
 
