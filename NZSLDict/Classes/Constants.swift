3eeffd4a00cda2675e37602e8e9ef726e0d654ee
Josh McArthur <joshua.mcarthur@gmail.com>
2017-06-09 15:34:57 +1200

    Add secondary text colour

--- NZSLDict/Classes/Constants.swift
+++ NZSLDict/Classes/Constants.swift
@@ -5,3 +5,4 @@
 let EntrySelectedName = "EntrySelected"
 let AppThemePrimaryColor = UIColor(red:0.16, green:0.50, blue:0.73, alpha:1.0)
 let AppThemeAccentColor = UIColor(red:0.96, green:0.47, blue:0.35, alpha:1.0)
+let AppSecondaryTextColour = UIColor(red:0.62, green:0.62, blue:0.62, alpha:1.0)
