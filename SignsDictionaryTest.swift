2b0f52fbbc9dacc9f05f72211344ef51c9942240
Eoin Kelly <eoin@rabidtech.co.nz>
2017-06-11 11:29:36 +1200

    Update swift syntax in tests

--- SignsDictionaryTest.swift
+++ SignsDictionaryTest.swift
@@ -24,39 +24,39 @@ class SignsDictionaryTest: XCTestCase {
     }
     
     func test_searchForExactMainGlossMatch() {
-        var results = signsDictionary.searchFor("Book");
-        let firstResult: DictEntry = results[0] as! DictEntry;
+        var results = signsDictionary.search(for: "Book");
+        let firstResult: DictEntry = results![0] as! DictEntry;
         assert(firstResult.gloss == "book");
     }
     
     func test_searchForExactMaoriGlossMatch() {
-        var results = signsDictionary.searchFor("ora");
-        let firstResult: DictEntry = results[0] as! DictEntry;
+        var results = signsDictionary.search(for: "ora");
+        let firstResult: DictEntry = results![0] as! DictEntry;
         assert(firstResult.gloss == "alive, live, survive");
     }
     
     func test_searchForContainsMainGloss() {
-        var results = signsDictionary.searchFor("classif");
-        let firstResult: DictEntry = results[0] as! DictEntry;
+        var results = signsDictionary.search(for: "classif");
+        let firstResult: DictEntry = results![0] as! DictEntry;
         assert(firstResult.gloss == "classifier");
     }
     
     func test_searchForContainsMaoriGloss() {
-        var results = signsDictionary.searchFor("akorang");
-        let firstResult: DictEntry = results[0] as! DictEntry;
+        var results = signsDictionary.search(for: "akorang");
+        let firstResult: DictEntry = results![0] as! DictEntry;
         assert(firstResult.gloss == "course");
     }
     
     func test_searchForExactSecondaryGloss() {
-        var results = signsDictionary.searchFor("nought");
-        let firstResult: DictEntry = results[0] as! DictEntry;
+        var results = signsDictionary.search(for: "nought");
+        let firstResult: DictEntry = results![0] as! DictEntry;
         assert(firstResult.gloss == "zero");
     }
     
     
     func test_searchForContainsSecondaryGloss() {
-        var results = signsDictionary.searchFor("not get involved, nothing to do with");
-        let firstResult: DictEntry = results[0] as! DictEntry;
+        var results = signsDictionary.search(for: "not get involved, nothing to do with");
+        let firstResult: DictEntry = results![0] as! DictEntry;
         assert(firstResult.gloss == "neutral");
     }
     
