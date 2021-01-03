aa9912d2ef7056cf163b819ad0044f921cfafb29
Josh McArthur <josh.mcarthur@ackama.com>
2021-01-03 21:57:59 +1300

    Apply padding from parent, not from the diagramview child

--- NZSLDict/Classes/DiagramView.swift
+++ NZSLDict/Classes/DiagramView.swift
@@ -6,18 +6,16 @@ class DiagramView: UIView {
 
     override init(frame: CGRect) {
         super.init(frame: frame)
-
-        let viewPadding = CGFloat(16.0)
         self.backgroundColor = UIColor(named: "app-background")
         self.autoresizingMask = [UIView.AutoresizingMask.flexibleWidth, UIView.AutoresizingMask.flexibleHeight]
 
-        detailView = DetailView(frame: CGRect(x: viewPadding, y: viewPadding, width: self.bounds.size.width - (viewPadding * 2), height: DetailView.height))
+        detailView = DetailView(frame: CGRect(x: 0, y: 0, width: self.frame.size.width, height: DetailView.height))
         detailView.autoresizingMask = UIView.AutoresizingMask.flexibleWidth
 
         self.addSubview(detailView)
 
-        imageView = UIImageView(frame: CGRect(x: viewPadding, y: detailView.frame.maxY, width: self.bounds.size.width - (viewPadding * 2), height: self.bounds.size.height-detailView.frame.maxY))
-        imageView.autoresizingMask = [UIView.AutoresizingMask.flexibleWidth, UIView.AutoresizingMask.flexibleHeight]
+        imageView = UIImageView(frame: CGRect(x: 0, y: detailView.frame.maxY, width: self.frame.size.width, height: self.frame.height-detailView.frame.maxY))
+        imageView.autoresizingMask = [UIView.AutoresizingMask.flexibleWidth]
         imageView.backgroundColor = UIColor(named: "app-background")
         imageView.contentMode = UIView.ContentMode.scaleAspectFit
 
